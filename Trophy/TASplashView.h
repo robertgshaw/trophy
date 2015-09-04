76daf2209cec0c3ace187950fd3e697e4e00a3a1
Robert Shaw <robertshaw@Roberts-MacBook-Pro-2.local>
2015-09-04 13:51:12 -0400

    started splash view

---
+++ Trophy/TASplashView.h
@@ -0,0 +1,13 @@
+//
+//  TASplashView.h
+//  Trophy
+//
+//  Created by Robert Shaw on 9/4/15.
+//  Copyright (c) 2015 Gigster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TASplashView : UIView
+
+@end
