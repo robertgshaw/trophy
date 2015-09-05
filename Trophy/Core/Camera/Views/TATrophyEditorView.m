b7148506213bc9892e3244570c2c90b55e9b5ce4
Matt Deveney <mattdeveney@Matts-MacBook-Pro-2.local>
2015-09-05 14:50:09 -0400

    camera editor redesign

--- Trophy/Core/Camera/Views/TATrophyEditorView.m
+++ Trophy/Core/Camera/Views/TATrophyEditorView.m
@@ -19,6 +19,8 @@ static const CGFloat kTrophySendIconWidth = 70.0;
 @property (nonatomic, strong) UIButton *closeButton;
 @property (nonatomic, strong) UITextField *captionTextField;
 @property (nonatomic, strong) UIButton *sendButton;
+@property (nonatomic, strong) UIView *backgroundView;
+@property (nonatomic, strong) UILabel *descriptionLabel;
 
 @end
 
@@ -33,22 +35,28 @@ static const CGFloat kTrophySendIconWidth = 70.0;
         self.backgroundImageView.contentMode = UIViewContentModeScaleAspectFill;
         [self addSubview:self.backgroundImageView];
         
-        _closeButton = [[UIButton alloc] initWithFrame:CGRectZero];
-        [self.closeButton setBackgroundImage:[UIImage imageNamed:@"back-icon"] forState:UIControlStateNormal];
-        [self.closeButton addTarget:self action:@selector(didPressClose) forControlEvents:UIControlEventTouchUpInside];
-        [self addSubview:self.closeButton];
-
+        _backgroundView = [[UIView alloc] initWithFrame:CGRectZero];
+        self.backgroundView.backgroundColor = [UIColor trophyNavyLessTranslucentColor];
+        self.backgroundView.hidden = YES;
+        [self addSubview:self.backgroundView];
+        
         _captionTextField = [[UITextField alloc] init];
         self.captionTextField.delegate = self;
         self.captionTextField.clearButtonMode = UITextFieldViewModeWhileEditing;
         self.captionTextField.autocapitalizationType = UITextAutocapitalizationTypeSentences;
         self.captionTextField.returnKeyType = UIReturnKeyDone;
-        self.captionTextField.backgroundColor = [UIColor highTranslucentWhite];
-
-        self.captionTextField.textColor = [UIColor trophyNavyColor];
+        self.captionTextField.backgroundColor = [UIColor clearColor];
+        self.captionTextField.tintColor = [UIColor whiteColor];
+        self.captionTextField.textColor = [UIColor whiteColor];
         self.captionTextField.textAlignment = NSTextAlignmentCenter;
+        self.captionTextField.font = [UIFont fontWithName:@"Avenir" size:24.0];
         self.captionTextField.hidden = YES;
         [self addSubview:self.captionTextField];
+        
+        _closeButton = [[UIButton alloc] initWithFrame:CGRectZero];
+        [self.closeButton setBackgroundImage:[UIImage imageNamed:@"closeup-close-icon"] forState:UIControlStateNormal];
+        [self.closeButton addTarget:self action:@selector(didPressClose) forControlEvents:UIControlEventTouchUpInside];
+        [self addSubview:self.closeButton];
 
         _sendButton = [[UIButton alloc] initWithFrame:CGRectZero];
         [self.sendButton setBackgroundImage:[UIImage imageNamed:@"trophy-send-icon"] forState:UIControlStateNormal];
@@ -71,20 +79,29 @@ static const CGFloat kTrophySendIconWidth = 70.0;
 
     self.backgroundImageView.frame = self.bounds;
     
-    
-
+    //lay out close button
     [self.closeButton sizeToFit];
     CGRect frame = self.closeButton.frame;
+    frame.size = CGSizeMake(45.0, 45.0);
     frame.origin.x = 30.0;
     frame.origin.y = 30.0;
     self.closeButton.frame = frame;
     
+    //lay out translucent background view
     frame.origin.x = 0.0;
-    frame.origin.y = 300.0;
+    frame.origin.y = 0.0;
     frame.size.width = CGRectGetWidth(self.bounds);
-    frame.size.height = 30.0;
+    frame.size.height = CGRectGetHeight(self.bounds);
+    self.backgroundView.frame = frame;
+    
+    //lay out text field
+    frame.size.width = CGRectGetWidth(self.bounds);
+    frame.size.height = 25;
+    frame.origin.x = CGRectGetMidX(self.bounds) - (frame.size.width / 2);
+    frame.origin.y = CGRectGetMidY(self.bounds) - (frame.size.height / 2);
     self.captionTextField.frame = frame;
 
+    //lay out send button
     [self.sendButton sizeToFit];
     frame = self.sendButton.frame;
     frame.origin.x = floorf((CGRectGetWidth(self.bounds) - kTrophySendIconWidth) / 2.0);
@@ -139,6 +156,8 @@ static const CGFloat kTrophySendIconWidth = 70.0;
             }
             [self.captionTextField resignFirstResponder];
         } else {
+            self.descriptionLabel.hidden = NO;
+            self.backgroundView.hidden = NO;
             self.captionTextField.hidden = NO;
             [self.captionTextField becomeFirstResponder];
         }
